cb33f95d25f17757db83eb5448b0f2a996936196
saipranithswargam <92095296+saipranithswargam@users.noreply.github.com>
2022-04-08 14:39:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function calculator(num1 ,num2 ,operator){
+    return operator(num1 ,num2);
+}
+function add(num1 ,num2){
+    return (Number(num1) + Number(num2));
+}
+function multiply(num1 ,num2){
+    return num1 * num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function divide(num1 , num2) {
+    return num1/num2;
+}
+function remainder(num1 ,num2){
+    return num1%num2;
+}
+num1=prompt("Enter first number " );
+num2=prompt("Enter Second number ");
+operation=prompt("enter the operation u need to perform(+,*,-,/)");
+var result=0;
+if(operation==="+"){
+    result=calculator(num1,num2,add);
+}
+else if(operation==="*"){
+    result=calculator(num1,num2,multiply);
+}
+else if(operation==="-"){
+    result=calculator(num1,num2,subtract);
+}
+else if(operation==="/"){
+    result=calculator(num1,num2,divide);
+}
+else if (operation==="%"){
+    result=calculator(num1,num2,remainder);
+}
+alert("result of operation is: "+result);
